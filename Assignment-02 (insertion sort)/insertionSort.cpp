a46adb5a32be00711905d010ef9674d4697eeba9
Mohit Singh <mohitkumarsingh9889@gmail.com>
2025-01-23 14:45:41 +0530

    updating loop

--- Assignment-02 (insertion sort)/insertionSort.cpp
+++ Assignment-02 (insertion sort)/insertionSort.cpp
@@ -1,48 +1,73 @@
 #include <iostream>
 #include <vector>
 #include <cstdlib>
-#include <algorithm>        //for rnad and srand
-#include <ctime>        
+#include <algorithm> //for rnad and srand
+#include <ctime>
+#include <fstream>
 using namespace std;
 
-void insertionSort(vector<int>& vec){
+int insertionSort(int *arr, int size)
+{
+    int comparision = 0;
     int key;
     int j;
-    for(int i=1;i<vec.size();i++){
-        key = vec[i];
-        j=i-1;
-        while(j>=0 && key < vec[j] ){
-            vec[j+1] = vec[j];
+    for (int i = 1; i < size; i++)
+    {
+        key = arr[i];
+        j = i - 1;
+        while (j >= 0 && key < arr[j])
+        {
+            comparision++;
+            arr[j + 1] = arr[j];
             j--;
         }
-        vec[j+1] = key;
+        arr[j + 1] = key;
     }
+    return comparision;
 }
-void printVector(vector<int> vec){
-    for(int i=0;i<vec.size();i++){
-        cout<<vec[i]<<" ";
+void printArray(int *arr, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
     }
-    cout<<endl;
+    cout << endl;
 }
-int main(){
+int main()
+{
     srand(time(0));
-    vector<int> vec={4,2,5,1,20,12};
-    printVector(vec);
-    insertionSort(vec);
-    printVector(vec);
 
     int n;
-    int minBound=1,maxBound=100;
-    cout<<"Enter data size : ";
-    cin>>n;
-    vector<int> nums(n,-1);
-    for(int i=0;i<10;i++){
-        for(int j=0;j<n;j++){
-            int randomNumber = minBound + rand()%(maxBound-minBound+1);
-            nums[j] = randomNumber;
+    int minBound = 1, maxBound = 100;
+    int *arr;
+
+    // file opening
+    ofstream csvFile("search_comparisons.csv");
+    csvFile << "Data Size,Best Case,Worst Case,Average Case\n";
+
+    for (int i = 10; i <= 100; i = i + 5)
+    {
+        int bestCase = INT_MAX;
+        int worstCase = INT_MIN;
+        int averageCase = 0;
+        for (int j = 0; j < 10; j++)
+        {
+
+            arr = new int[i];
+            for (int k = 0; k < i; k++)
+            {
+                int randomNumber = minBound + rand() % (maxBound - minBound + 1);
+                arr[k] = randomNumber;
+            }
+            int compare = insertionSort(arr, i);
+            bestCase = bestCase > compare ? compare : bestCase;
+            worstCase = worstCase > compare ? worstCase : compare;
+            averageCase += compare;
+            // printArray(arr, i);
         }
-        printVector(nums);
-        
+
+        // pushing data in the file
+        csvFile << i << "," << bestCase << "," << worstCase << "," << averageCase / i << "\n";
     }
     return 0;
 }
